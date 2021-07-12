6b61134756b014ae32c023545355a3e4028208ed
Mike Sannikov <atarity@gmail.com>
2021-07-12 12:06:48 +0300

    Revert "arduino-ide with control surface lib"
    This reverts commit f9d82cb4c6520de99ddae348f83b22c460de9109.

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # xiao-knob
-Smallest USB-C encoder 🎛  in 8 min for $8
-
-## Platformio
-
-## Arduino IDE
-- install [Control Surface](https://github.com/tttapa/Control-Surface) library
-- install [MIDIUSB](https://github.com/arduino-libraries/MIDIUSB) library
+Smallest USB-C encoder 🎛  in 5 min for $5
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,81 @@
 #include <Arduino.h>
-#include <Control_Surface.h>
 
-USBMIDI_Interface midi; // Instantiate a MIDI over USB interface
+#define buttonPin 8
+#define buttonGndPin 10
+#define encoderAPin 3
+#define encoderGndPin 4
+#define encoderBPin 5
 
-// Instantiate a CCButton object
-CCButton button {
-    // Push button on pin 8:
-    8,
-    // General Purpose Controller #1 on MIDI channel 1:
-    {MIDI_CC::General_Purpose_Controller_1, CHANNEL_1},
-};
+volatile int buttonState = 0; // volatile because will use in interruption function
 
-CCRotaryEncoder enc {
-  {3, 5},       // pins
-  MCU::V_POT_1, // MIDI address (CC number + optional channel)
-  1,            // optional multiplier if the control isn't fast enough
-};
+volatile int encoderPos = 0;  // a counter for the dial
+int lastReportedPos = 1;   // change management
+static boolean rotating = false;    // debounce management
 
+// interrupt service routine vars
+boolean A_set = false;
+boolean B_set = false;
+
+void doEncoderA() {
+    if ( rotating ) delay (1);  // wait a little until the bouncing is done
+    if ( digitalRead(encoderAPin) != A_set ) { // debounce once more
+        A_set = !A_set;
+        // adjust counter + if A leads B
+        if ( A_set && !B_set )
+        encoderPos++;
+        rotating = false;  // no more debouncing until loop() hits again
+    }
+}
+
+void doEncoderB() {
+    if ( rotating ) delay (1);
+    if ( digitalRead(encoderBPin) != B_set ) {
+        B_set = !B_set;
+        //  adjust counter - 1 if B leads A
+        if ( B_set && !A_set )
+        encoderPos--;
+        rotating = false;
+    }
+}
+
+void doClick() {
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        //encoderPos = 0;
+    } else {
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+}
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-    pinMode(10, OUTPUT);
-    digitalWrite(10, LOW);   // setting pin to GND
+    pinMode(LED_BUILTIN, OUTPUT);
 
-    RelativeCCSender::setMode(relativeCCmode::MACKIE_CONTROL_RELATIVE);
-    Control_Surface.begin(); // Initialize Control Surface
+    // encoder button setup
+    pinMode(buttonPin, INPUT_PULLUP);
+    pinMode(buttonGndPin, OUTPUT);
+    digitalWrite(buttonGndPin, LOW);   // setting pin to GND
+    attachInterrupt(digitalPinToInterrupt(buttonPin), doClick, CHANGE);  // Button click on interrupt 8
+
+    // encoder data pins setup
+    pinMode(encoderAPin, INPUT_PULLUP);
+    pinMode(encoderBPin, INPUT_PULLUP);
+    digitalWrite(encoderAPin, HIGH);
+    digitalWrite(encoderBPin, HIGH);
+    pinMode(encoderGndPin, OUTPUT);
+    digitalWrite(encoderGndPin, LOW);
+    attachInterrupt(digitalPinToInterrupt(encoderAPin), doEncoderA, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(encoderBPin), doEncoderB, CHANGE);
+
+    Serial.begin (115200);
 }
 
 void loop() {
-    Control_Surface.loop(); // Update the Control Surface
+    rotating = true;  // reset the debouncer
+    if (lastReportedPos != encoderPos) {
+        Serial.print("Index: ");
+        Serial.println(encoderPos, DEC);
+        lastReportedPos = encoderPos;
+    }
 }
