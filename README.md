ab30669bc61dfe2e5c8f2d145917514526de9209
Mike Sannikov <atarity@gmail.com>
2021-07-12 16:08:13 +0300

    platformio fixed

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Smallest USB-C encoder 🎛  in 8 min for $8
 
 ## Platformio
+- Install [Control Surface](https://github.com/tttapa/Control-Surface) library from **master** like `pio lib install https://github.com/tttapa/Control-Surface.git#master`. It is essential to use latest version which is [well- documented](https://tttapa.github.io/Control-Surface-doc/Doxygen/d2/d1f/RotaryEncoder_8ino-example.html) (check the version of the doc first).
+- Install [MIDIUSB](https://github.com/arduino-libraries/MIDIUSB) library thru `pio`
 
 ## Arduino IDE
 - install [Control Surface](https://github.com/tttapa/Control-Surface) library
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,9 @@
 
 USBMIDI_Interface midi; // Instantiate a MIDI over USB interface
 
-// Instantiate a CCButton object
 CCButton button {
-    // Push button on pin 8:
-    8,
-    // General Purpose Controller #1 on MIDI channel 1:
-    {MIDI_CC::General_Purpose_Controller_1, CHANNEL_1},
+    8,      // Push button on pin 8
+    {MIDI_CC::General_Purpose_Controller_1, CHANNEL_1},     // General Purpose Controller #1 on MIDI channel 1
 };
 
 CCRotaryEncoder enc {
@@ -21,6 +18,8 @@ void setup() {
     pinMode(10, OUTPUT);
     digitalWrite(10, LOW);   // setting pin to GND
 
+    pinMode(4, OUTPUT);
+    digitalWrite(4, LOW);   // setting central encoder pin to ground
     RelativeCCSender::setMode(relativeCCmode::MACKIE_CONTROL_RELATIVE);
     Control_Surface.begin(); // Initialize Control Surface
 }
